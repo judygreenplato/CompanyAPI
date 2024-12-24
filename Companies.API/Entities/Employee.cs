6552e53ca87c0efbe932cafcc58f12c45b36ed77
Judygreenchan <chaneparambil893@gmail.com>
2024-12-24 12:22:19 +0100

    Added Entities

---
+++ Companies.API/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Companies.API.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Age is a required field.")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string? Position { get; set; }
+        public int CompanyId { get; set; }
+        //Navigation Property
+        public Company? Company { get; set; }
+    }
+}
